c963e2a1c9b3bb8c4a8d879552f494a58f8d8399
archae0pteryx <jesus.christ@openmailbox.org>
2017-07-11 21:31:38 -0700

    rendering cal in a span

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,31 +1,18 @@
 import React from 'react'
 import cheerio from 'cheerio'
 import axios from 'axios'
-
+import {Spinner} from './Spinner'
 const baseURL = 'https://crossorigin.me/https://github.com/'
 
-const opts = {
-    headers: {
-		// "Authorization": 'token 3b5bc180d175e8c76622681daee9944e450eb327',
-		// 'X-Requested-With': 'XMLHttpRequest',
-		// 'Access-Control-Allow-Origin': '*',
-		// 'Access-Control-Allow-Methods': 'GET',
-		// 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-		// 'Content-Type': 'application/json'
-	}
-};
 function getSVG(user) {
     return new Promise((resolve, reject) => {
         const url = baseURL + user
-        axios.get(url, opts).then(res => {
-            const load = cheerio.load(res.data)
-            const parsed = load('.js-calendar-graph').html()
-            const svg = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 720 120"> ' + parsed + ' </svg>';
-            // fs.writeFile('./scrapedSVG.svg', svg, function(err) {
-            //     console.log('File successfully written!')
-            // })
-            resolve(svg)
-        }).catch(err => reject(err))
+		axios.get(url).then(res => {
+			const load = cheerio.load(res.data)
+			const parsed = load(".js-calendar-graph").html()
+			const svg = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 720 120"> ' + parsed + ' </svg>';
+			resolve(svg)
+		}).catch(err => reject(err));
     })
 }
 
@@ -33,20 +20,25 @@ export default class extends React.Component {
 	constructor() {
 		super()
 		this.state = {
-			svg: ''
-		}
+			svg: '',
+			loading: true
+		};
 	}
 	componentWillMount() {
 		getSVG('archae0pteryx').then(svg => {
-			console.log("SVG ", svg)
-			this.setState({svg: svg})
-		})
+			// console.log("SVG ", svg)
+			this.setState({ svg: svg, loading: false });
+		});
 	}
 	render() {
-		const {svg} = this.state
+		const { svg, loading } = this.state;
 		return (
 			<div>
-				<img src={svg} alt="github contrib cal"/>
+				{loading ?
+					<Spinner/>
+					: 
+					<span dangerouslySetInnerHTML={{__html: svg}} />
+				}
 			</div>
 		)
 	}
---
+++ src/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Anime = styled.div`
+  background: black;
+  color: aliceblue;
+`
+
+export const Spinner = () => (
+  <Anime>Loading...</Anime>
+)
