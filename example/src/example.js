414e2cd0a7963b16c6ae362019ca776f46e1d158
archae0pteryx <jesus.christ@openmailbox.org>
2017-07-11 20:48:20 -0700

    corrected misspelling of calander

--- example/src/example.js
+++ example/src/example.js
@@ -1,12 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var Calander = require('react-ghcal');
+var Calendar = require('react-ghcal');
 
 var App = React.createClass({
 	render () {
 		return (
 			<div>
-				<Calander />
+				<Calendar />
 			</div>
 		);
 	}
--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var initGulpTasks = require('react-component-gulp-tasks');
 var taskConfig = {
 
 	component: {
-		name: 'Calander',
+		name: 'Calendar',
 		dependencies: [
 			'classnames',
 			'react',
